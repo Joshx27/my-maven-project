d57175fd49233be6f04140dac4d9d0cd58c2f4e4
Joshua Hill <98056388+Joshx27@users.noreply.github.com>
2023-10-21 16:47:58 -0500

    added functions to prim

--- src/main/java/com/example/PrimsParallelMST.java
+++ src/main/java/com/example/PrimsParallelMST.java
@@ -5,7 +5,36 @@ import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 public class PrimsParallelMST {
-    public static void main(String[] args) {
+
+    // number of nodes
+    int n;
+
+    // edges assigned to each node
+    List<List<Integer>> edges;
+
+    // vector containing edges for the minimum spanning tree
+    int[] G;
+
+    public PrimsParallelMST(int n, List<List<Integer>> edges, int[] G) {
+        this.n = n;
+        this.edges = edges;
+        G = new int[n];
+        this.G = G;
+    }
+
+    boolean fixed(int j, int[] G) {
+
+    }
+
+    boolean forbidden(int j) {
+
+    }
+
+    void advance(int j) {
+
+    }
+
+    int[] LLPPrim() {
         // Create an ExecutorService with a single thread
         ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
@@ -21,4 +50,9 @@ public class PrimsParallelMST {
         // Schedule the task to run repeatedly with a fixed delay
         executor.scheduleWithFixedDelay(alwaysTask, 0, 1, TimeUnit.SECONDS);
     }
+
+    public static void main(String[] args) {
+
+    }
+
 }
