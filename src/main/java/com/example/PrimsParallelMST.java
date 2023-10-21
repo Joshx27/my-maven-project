49576c7443809d5f76e1345f3f2bcc6a0809dad0
Joshua Hill <98056388+Joshx27@users.noreply.github.com>
2023-10-21 15:39:40 -0500

    prim update

--- src/main/java/com/example/PrimsParallelMST.java
+++ src/main/java/com/example/PrimsParallelMST.java
@@ -1,5 +1,24 @@
 package com.example;
 
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.IntStream;
+
 public class PrimsParallelMST {
-    
+    public static void main(String[] args) {
+        // Create an ExecutorService with a single thread
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+        // Define a task to be executed repeatedly
+        Runnable alwaysTask = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("This task runs always!");
+                // You can put your code here
+            }
+        };
+
+        // Schedule the task to run repeatedly with a fixed delay
+        executor.scheduleWithFixedDelay(alwaysTask, 0, 1, TimeUnit.SECONDS);
+    }
 }
