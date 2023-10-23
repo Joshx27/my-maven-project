061a31b6920c831651dbca535ed102e75317352b
Joshx27 <joshuajhill2002@gmail.com>
2023-10-23 09:12:31 -0500

    update

--- src/main/java/com/example/ParallelPrefix.java
+++ src/main/java/com/example/ParallelPrefix.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class ParallelPrefix {
 
-    private void main(String args[]) {
+    public static void main(String args[]) {
         System.out.println("egg");
     }
 }
