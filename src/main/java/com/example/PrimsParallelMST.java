062a7badcb35545d6c54018b5585e2ffc8721c85
Joshx27 <joshuajhill2002@gmail.com>
2023-10-23 08:54:48 -0500

    update prim

--- src/main/java/com/example/PrimsParallelMST.java
+++ src/main/java/com/example/PrimsParallelMST.java
@@ -1,58 +1,81 @@
 package com.example;
 
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Edge implements Comparable<Edge> {
+    int to, weight;
+
+    public Edge(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return Integer.compare(this.weight, other.weight);
+    }
+}
 
 public class PrimsParallelMST {
 
-    // number of nodes
-    int n;
+    public static List<Edge> primMST(List<List<Edge>> graph) {
+        int numVertices = graph.size();
+        List<Edge> minimumSpanningTree = new ArrayList<>();
+        boolean[] visited = new boolean[numVertices];
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>();
 
-    // edges assigned to each node
-    List<List<Integer>> edges;
+        // Start with the first vertex (vertex 0)
+        visited[0] = true;
 
-    // vector containing edges for the minimum spanning tree
-    int[] G;
+        for (Edge edge : graph.get(0)) {
+            priorityQueue.add(edge);
+        }
 
-    public PrimsParallelMST(int n, List<List<Integer>> edges, int[] G) {
-        this.n = n;
-        this.edges = edges;
-        G = new int[n];
-        this.G = G;
-    }
+        while (minimumSpanningTree.size() < numVertices - 1) {
+            Edge minEdge = priorityQueue.poll();
+            int to = minEdge.to;
 
-    boolean fixed(int j, int[] G) {
-
-    }
-
-    boolean forbidden(int j) {
-
-    }
-
-    void advance(int j) {
-
-    }
-
-    int[] LLPPrim() {
-        // Create an ExecutorService with a single thread
-        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-
-        // Define a task to be executed repeatedly
-        Runnable alwaysTask = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("This task runs always!");
-                // You can put your code here
+            if (visited[to]) {
+                continue;
             }
-        };
 
-        // Schedule the task to run repeatedly with a fixed delay
-        executor.scheduleWithFixedDelay(alwaysTask, 0, 1, TimeUnit.SECONDS);
+            visited[to] = true;
+            minimumSpanningTree.add(minEdge);
+
+            for (Edge edge : graph.get(to)) {
+                if (!visited[edge.to]) {
+                    priorityQueue.add(edge);
+                }
+            }
+        }
+
+        return minimumSpanningTree;
     }
 
     public static void main(String[] args) {
+        int numVertices = 4;
+        List<List<Edge>> graph = new ArrayList<>();
 
+        for (int i = 0; i < numVertices; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        // Add edges to the graph
+        graph.get(0).add(new Edge(1, 2));
+        graph.get(1).add(new Edge(0, 2));
+        graph.get(1).add(new Edge(2, 3));
+        graph.get(2).add(new Edge(1, 3));
+        graph.get(0).add(new Edge(3, 4));
+        graph.get(3).add(new Edge(0, 4));
+
+        List<Edge> minimumSpanningTree = primMST(graph);
+
+        System.out.println("Minimum Spanning Tree Edges:");
+        for (Edge edge : minimumSpanningTree) {
+            System.out.println("(to: " + edge.to + ", weight: " + edge.weight + ")");
+        }
     }
 
 }
